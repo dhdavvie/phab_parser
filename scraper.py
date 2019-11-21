222921e1d96b492f5b394820268e9ca6db7e5db8
David Heiberg <dheiberg@mozilla.com>
2019-11-21 17:56:31 +0000

    Event type mapping, reordering methods

--- scraper.py
+++ scraper.py
@@ -7,13 +7,33 @@ URL = "https://phabricator.services.mozilla.com/feed/transactions/"
 BASE_URL = "https://phabricator.services.mozilla.com"
 
 
-def map_feed_tuple(feed_tuple):
-    story_phid, feed_story = feed_tuple
-    feed_story.update({"storyPHID": story_phid})
-    return feed_story
-
-
 class PhabEventListener:
+
+    blacklist = ["added inline comments to D",
+                 "added a comment to D",
+                 "added a reviewer for D",
+                 "removed a reviewer for D",
+                 "requested review of D",
+                 "requested changes to D",
+                 "added a subscriber to D",
+                 "added a project to D",
+                 "committed rMOZILLA",
+                 "committed rVCT",
+                 "committed rCIADMIN",
+                 "edited reviewers for D",
+                 "updated the summary of D",  # Maybe useful to upstream info?
+                 "accepted D",  # Maybe useful to upstream info?
+                 "retitled D",  # Maybe useful to upstream info?
+                 "blocking reviewer(s) for D",
+                 "planned changes to D",
+                 "resigned from D"]
+
+    event_mapping = {
+        "updated the diff for D": "commit",
+        "created D": "commit",
+        "closed D": "closed",
+        "abandoned D": "closed",  # Bit unsure about this one
+    }
 
     def __init__(self, config):
         self.running = True
@@ -22,13 +42,6 @@ class PhabEventListener:
         self.timer_in_seconds = config['phabricator']['listener']['interval']
         self.queue = []
         self.latest = None
-        self.blacklist = ["added inline comments to D",
-                          "added a comment to D",
-                          "added a reviewer for D",
-                          "removed a reviewer for D",
-                          "requested review of D",
-                          "added a subscriber to D",
-                          "added a project to D"]
         self.datetime_format = "%a, %b %d, %I:%M %p"
 
         self.phab = Phabricator(host='https://phabricator.services.mozilla.com/api/',
@@ -60,7 +73,7 @@ class PhabEventListener:
             if result.response:
                 print("Response: %s" % result.response)
                 results = sorted(result.response.items(), key=chrono_key)
-                results = map(map_feed_tuple, results)
+                results = map(self.map_feed_tuple, results)
                 feed.extend(results)
                 if len(results) == 100 and before is not None:
                     # There may be more events we wish to fetch
@@ -71,9 +84,6 @@ class PhabEventListener:
         return feed
 
     def parse(self, feed):
-        latest = self.latest
-        caught_up = False
-
         # Go through rows in reverse order, and ignore first row as it has the table headers
         for event in feed:
 
@@ -81,7 +91,12 @@ class PhabEventListener:
             event_text = re.compile("[0-9]{5}:").split(event['text'])[0]
 
             # Check if this is an event we wish to ignore
-            if any([event_type in event_text for event_type in self.blacklist]):
+            if any([event_type in event_text for event_type in PhabEventListener.blacklist]):
+                continue
+
+            # Map the event text to an event type so we know how to handle it
+            event['type'] = self.map_event_type(event_text)
+            if event['type'] is None:
                 continue
 
             # Add the event to the queue, and set this as the latest parsed
@@ -89,8 +104,20 @@ class PhabEventListener:
             self.latest = event
             print("Event", event['text'])
 
-        # Update the latest to be the last event we parsed here
-        self.latest = latest
+    @staticmethod
+    def map_event_type(event_text):
+        for event_type, mapping in PhabEventListener.event_mapping.items():
+            if event_type in event_text:
+                return mapping
+
+        print("Unknown event type: %s" % event_text)
+        # new relic metric?
+
+    @staticmethod
+    def map_feed_tuple(feed_tuple):
+        story_phid, feed_story = feed_tuple
+        feed_story.update({"storyPHID": story_phid})
+        return feed_story
 
 
 def run_phabricator_listener(config):
